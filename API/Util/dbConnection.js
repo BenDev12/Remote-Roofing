27002aa1af01ae965e1036b9a36cbd4d6681b82d
BenDev12 <61835307+benardonyango-oss@users.noreply.github.com>
2020-06-02 22:02:45 +0300

    Created database  connection string

---
+++ API/Util/dbConnection.js
@@ -0,0 +1,15 @@
+const env = require("./env");
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(env.db_name, env.db_username, env.db_password, {
+  host: env.db_host,
+  dialect: env.db_dialect,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idel: 10000,
+  },
+});
+module.exports = sequelize;
---
+++ API/Util/env.js
@@ -0,0 +1,11 @@
+module.exports = {
+  port: "5000",
+  appEndpoint: "http://localhost:5000",
+  apiEndpoint: "http://localhost:5000",
+  environment: "env",
+  db_name: "remotenode",
+  db_username: "postgres",
+  db_password: "m@th3wcoder",
+  db_host: "localhost",
+  db_dialect: "postgres",
+};
